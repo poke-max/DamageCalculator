16032c0ad199254755e3b8c2695619bf552a8ca8
poke-max <61891131+poke-max@users.noreply.github.com>
2020-03-10 16:53:53 -0300

    Update main.js

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ function rango()
     else {s = 1;}
   }
   var pr=Number(document.getElementById('pru').value);
-  var res=atot*ptot*ob*au*c*s/pr;
+  var res=Math.floor(parseInit(atot*ptot*c/pr)*ob*au*s);
   document.getElementById('res').value = res;
   var red=Math.floor(parseInt(atot*ptot/pr)*ob*au*c*s);
   document.getElementById('red').value = red;
